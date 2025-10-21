a73c9c782b77ec11bd3f5eef4ffd30501dfcb753
Erik Dahl <erik.dahl@realpage.com>
2025-10-21 09:29:47 -0500

    work in progress for oauth

--- CarvedRock.Mcp/CarvedRockTools.cs
+++ CarvedRock.Mcp/CarvedRockTools.cs
@@ -7,10 +7,12 @@ namespace CarvedRock.Mcp;
 public class CarvedRockTools(IHttpClientFactory httpClientFactory)
 {
     [McpServerTool(Name = "get_products"), Description("Get a list of all available products.")]
-    public async Task<List<ProductModel>> GetAllProductsAsync(CancellationToken cancellationToken = default)
+    public async Task<List<ProductModel>> GetAllProductsAsync(
+        CancellationToken cancellationToken = default)
     {
         var client = httpClientFactory.CreateClient("CarvedRockApi");
-        var response = await client.GetFromJsonAsync<List<ProductModel>>("/product", cancellationToken);
+        var response = await client.GetFromJsonAsync<List<ProductModel>>("/product", 
+            cancellationToken);
         return response ?? [];
     }
 
--- CarvedRock.Mcp/Program.cs
+++ CarvedRock.Mcp/Program.cs
@@ -1,11 +1,64 @@
 using CarvedRock.Mcp;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using ModelContextProtocol.AspNetCore.Authentication;
+using ModelContextProtocol.Authentication;
+
+const string McpServerUrl = "http://localhost:5241";
+const string OAuthServerUrl = "https://demo.duendesoftware.com";
+
+var backChannelHttpClient = new HttpClient
+{
+    BaseAddress = new Uri(OAuthServerUrl)
+};
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("DevAll", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        policy.WithExposedHeaders("mcp-session-id", "last-event-id", "mcp-protocol-version");          
+    });
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultChallengeScheme = McpAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+        .AddJwtBearer(options =>
+        {
+            options.Backchannel = backChannelHttpClient;
+            options.Authority = OAuthServerUrl;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidAudience = McpServerUrl,
+                ValidIssuer = OAuthServerUrl,
+                NameClaimType = "name",
+                RoleClaimType = "roles"
+            };
+        })
+        .AddMcp(options =>
+        {
+            options.ResourceMetadata = new ProtectedResourceMetadata
+            {
+                Resource = new Uri(McpServerUrl),
+                AuthorizationServers = { new Uri(OAuthServerUrl) },
+                ScopesSupported = ["api", "openid", "profile", "email", "offline_access"]
+            };
+        });
+builder.Services.AddAuthorization();
+
 builder.Services.AddMcpServer()
-    .WithHttpTransport()    
+    .WithHttpTransport()     
     .WithTools<CarvedRockTools>()
     .WithTools<AdminTools>();
 
@@ -14,7 +67,23 @@ builder.Services.AddHttpClient("CarvedRockApi", client =>
 
 var app = builder.Build();
 
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors("DevAll");   
+}
+
 app.MapDefaultEndpoints();
-app.MapMcp();
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+var mcpEndpoint = app.MapMcp();
+
+if (app.Environment.IsDevelopment())
+{
+    mcpEndpoint.RequireCors("DevAll");
+}
+//mcpEndpoint.RequireAuthorization();
 
 app.Run();
